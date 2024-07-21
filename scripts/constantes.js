2c7cd6111297ae66b8b4534c4055d57660e4768e
Rubia-Souza <rubia.alice.m.s@gmail.com>
2024-07-20 23:28:27 -0300

    feat: Adiciona arquivos constantes

---
+++ scripts/constantes.js
@@ -0,0 +1,11 @@
+// Aceleração da gravidade dado em m/s²
+const VALOR_GRAVIDADE_PADRAO = 9.8;
+
+// Velocidade inicial dada em m/s
+const VALOR_VELOCIDADE_INICIAL_PADRAO = 30;
+
+// Angulo de lançamento dado em graus (°)
+const VALOR_ANGULO_LANCAMENTO_PADRAO = 45;
+
+// Valor do tempo de atualização dado em segundos (s)
+const VALOR_TEMPO_ATUALIZACAO_PADRAO = 0.05;
